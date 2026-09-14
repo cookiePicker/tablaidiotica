ca23cff1e17be238f03e1237437b8cd338c40f73
Hnotreal <154547642+cookiePicker@users.noreply.github.com>
2026-09-14 22:53:57 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return send_from_directory("static", "index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("index.html")
